b751d8018b2cab68f283ee06050237e80d44abed
Limmy <y12341@yandex.ru>
2017-01-17 19:36:09 +0200

    DBStrategy is a half-completed. Only update and close features are left.

--- src/net/lim/strategies/DBStrategy.java
+++ src/net/lim/strategies/DBStrategy.java
@@ -2,6 +2,8 @@ package net.lim.strategies;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.sql.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -9,26 +11,59 @@ import java.util.logging.Logger;
  */
 public class DBStrategy implements Strategy, Closeable {
 
+
     private static DBStrategy instance;
     private static final Logger DBLogger = Logger.getLogger(DBStrategy.class.getName());
-    //add a connection here
+    private Connection connection;
+    private final String url = "jdbc:mysql://localhost:3306/projectb_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final String user = "root";
+    private final String password = "root";
 
     private DBStrategy() {
         //connect to a database, if it isn't success, send information to controller (for using a XLSStrategy)
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            DBLogger.log(Level.INFO, "Connection is created");
+        }
+        catch (SQLException e) {
+            DBLogger.log(Level.SEVERE, "Can't create a connection to the Database");
+            e.printStackTrace();
+        }
+
     }
 
-    public static DBStrategy getInstance() {
+    public static DBStrategy getInstance() throws SQLException {
         if (instance == null) instance = new DBStrategy();
         return instance;
     }
 
+    public static void main(String[] args) throws SQLException{
+        DBStrategy strategy = getInstance();
+        System.out.println(strategy.read("MISTERX"));
+
+    }
+
     public void create(String name) {
-        //INSERT INTO
+        try (Statement createStatement = connection.createStatement()){
+            createStatement.execute("INSERT INTO users (userName, attempts) VALUES (\"" + name + "\" , 1)");
+        } catch (SQLException e) {
+            DBLogger.log(Level.WARNING, "Can't write a user row into the database");
+            e.printStackTrace();
+        }
     }
 
     public int read(String name) {
-        //SELECT attempts FROM USERS
-        //WHERE userName = name
+        try (Statement selectStatement = connection.createStatement()) {
+            ResultSet set = selectStatement.executeQuery("SELECT attempts FROM users WHERE userName = \"" + name + "\";");
+            if (set.next()) {
+                return set.getInt(1);
+            }
+            else DBLogger.log(Level.INFO, "This user is not exist");
+        }
+        catch (SQLException e) {
+            DBLogger.log(Level.WARNING, "Can't read a number of attempts. Return 0");
+            e.printStackTrace();
+        }
         return 0;
     }
 
